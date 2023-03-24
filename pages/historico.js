edf971d4cbc2b572d31ad198788c07c8483c3d07
nandadegcarvalho <nandadeg@icloud.com>
2023-03-24 11:50:11 -0300

    git

---
+++ pages/historico.js
@@ -0,0 +1,8 @@
+export default function HistoricoPage() {
+    return (
+        <div>
+            <h1>Meu histórico de Projetos</h1>
+            <a href="/"> Ir para Home</a>
+        </div>  
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+    <div>
+        <h1>Minha Primeira Home Page</h1>
+        <a href="/historico"> Ir para Histórico</a>
+    </div>  
+)
+}
